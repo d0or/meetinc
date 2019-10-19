f0c4f7e3bf6eaaa93d472581b6a0fca485466865
Sebastian Heyden <sebastian@heyden.eu>
2019-10-20 01:14:08 +0200

    update smart contract

--- contracts/contracts/Meeting.sol
+++ contracts/contracts/Meeting.sol
@@ -2,70 +2,90 @@ pragma solidity ^0.5.8;
 
 contract Meeting {
 
-  struct EventStruct {
+  /*struct EventStruct {
     address[] rsvps;
-    bool isRegistered;
+    bytes cid;
+
     bool isCanceled;
   }
 
-  mapping(bytes32 => EventStruct) public events;
+  mapping(bytes32 => EventStruct) public events;*/
 
   mapping(bytes32 => uint8) public meetups;
   mapping(bytes32 => uint8) public attendees;
 
-  event MeetupCreated(string url);
+  event MeetupCreated(string url, bytes cid);
+  event MeetupRSVPee(string url, address attendee);
+
+  function rsvpForEvent(string memory url, bytes memory cid) public {
+    bytes32 id = keccak256(abi.encode(url));
+
+    meetups[id] = meetups[id] + 1; // useful for count and cheap existence check
+
+    if (meetups[id] > 1) {
+      // events[id].rsvps.push(msg.sender);
+    } else {
+      // events[id] = EventStruct(new address[](0), cid, false); // useful for detail information
+      // events[id].rsvps.push(msg.sender);
+
+      emit MeetupCreated(url, cid);
+    }
+
+    bytes32 attend = keccak256(abi.encode(url, msg.sender));
+    attendees[attend] = 2; // code for attending
+
+    emit MeetupRSVPee(url, msg.sender);
+  }
 
   function rsvpForEvent(string memory url) public {
     bytes32 id = keccak256(abi.encode(url));
+    require(meetups[id] > 0, "meetup needs to be created first");
 
-    if (events[id].isRegistered) {
-      events[id].rsvps.push(msg.sender);
-    } else {
-      events[id] = EventStruct(new address[](0), true, false);
-      events[id].rsvps.push(msg.sender);
+    // events[id].rsvps.push(msg.sender);
 
-      emit MeetupCreated(url);
-    }
+    bytes32 attend = keccak256(abi.encode(url, msg.sender));
+    attendees[attend] = 2;
+
+    emit MeetupRSVPee(url, msg.sender);
   }
 
-  function getAttendees(string memory url) public view returns (address[] memory attendants) {
-    bytes32 id = keccak256(abi.encode(url));
+  function isAttending(string memory url) public view returns (bool attending) {
+    attending = isAttending(url, msg.sender);
+  }
 
-    if (events[id].isRegistered) {
+  function isAttending(string memory url, address identity) public view returns (bool attending) {
+    bytes32 attend = keccak256(abi.encode(url, identity));
+    attending = attendees[attend] == 2;
+  }
+
+  function getAttendees(bytes32 id) public view returns (address[] memory attendants) {
+    if (meetups[id] > 0) {
       attendants = events[id].rsvps;
     } else {
       attendants = new address[](0);
     }
   }
 
+  function getAttendees(string memory url) public view returns (address[] memory attendants) {
+    bytes32 id = keccak256(abi.encode(url));
+    attendants = getAttendees(id);
+  }
+
+  function isRegistered(bytes32 id) public view returns (bool registered) {
+      registered = meetups[id] > 0;
+  }
+
   function isRegistered(string memory url) public view returns (bool registered) {
     bytes32 id = keccak256(abi.encode(url));
-
-    registered = events[id].isRegistered || false;
-  }
-  
-  function naiveAttention(string memory url) public {
-    bytes32 id = keccak256(abi.encode(url));
-
-    meetups[id] = meetups[id] + 1;
-
-    if (meetups[id] == 1) {
-      emit MeetupCreated(url);
-    }
-
-    bytes32 attend = keccak256(abi.encode(url, msg.sender));
-
-    attendees[attend] = 2;
+    registered = isRegistered(id);
   }
 
-  function doesAttend(string memory url) public view returns (bool isAttending) {
-    bytes32 attend = keccak256(abi.encode(url, msg.sender));
-    isAttending = attendees[attend] == 2;
-  }
-  
-  function count(string memory url) public view returns (uint users) {
-    bytes32 id = keccak256(abi.encode(url));
+  function countAttendees(bytes32  id) public view returns (uint users) {
     users = meetups[id];
   }
 
+  function countAttendees(string memory url) public view returns (uint users) {
+    bytes32 id = keccak256(abi.encode(url));
+    users = countAttendees(id);
+  }
 }
