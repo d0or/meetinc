a7f5c93f581095578a745991bff051d09953de92
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-10-20 01:46:57 +0200

    fake gql api

--- event-wallet/src/pages/Home.js
+++ event-wallet/src/pages/Home.js
@@ -1,31 +1,64 @@
 import React, { useEffect, useState } from 'react'
 
-import provider from '../lib/provider'
+import { client, getAllEvents } from "../lib/gqlClient.js"
 
+//import abi from '../../../contracts/build/contracts/Meeting.json'
+
+import { provider, wallet } from '../lib/provider'
 
 const events =
     [
         {
-            title: 'Diffusion 2001',
-            date: '2019-11-01'
+            id: '123-456',
+            url: 'https://meetup.com/test',
+            title: 'ding',
+            starts: '2019-11-01',
+            ends: '2019-11-05',
+            image: '',
+            venue: {
+                location: {
+
+                }
+            }
         }
     ]
 
 export default () => {
     const [balance, setBalance] = useState(0)
+    const [meetups, setMeetups] = useState([])
 
-    useEffect(() => {
-        provider.getGasPrice().then((gasPrice) => {
-            const gasPriceString = gasPrice.toString();
+    /*useEffect(() => {
 
-            console.log("Current gas price: " + gasPriceString);
-        });
+        (async () => {
+            const accounts = await provider.listAccounts();
+            const acc0 = accounts[0]
+            const b = await provider.getBalance(acc0)
+
+            setBalance(b.toString())
+
+        })()
 
         return function cleanup() { }
-    });
+
+    });*/
+    useEffect(() => {
+
+        (async () => {
+            const evts = await getAllEvents()
+            setMeetups(evts)
+            console.log(evts)
+
+        })()
+
+        return function cleanup() { }
+
+    }, []);
+
 
     return <div>
-        <h2>Welcome!</h2>
-
+        <h2>Your balance is none !</h2>
+        <ul>
+            {meetups.map(m => (<li key={m.id}>{m.url} {m.ipfs.title}</li>))}
+        </ul>
     </div>
 }
