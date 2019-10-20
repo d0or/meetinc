864e3667bafa4aa352818b67ba32f08b7ae4d64c
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-10-20 09:31:43 +0200

    using the new abi added ethers & browser based ipfs to the party plugin: showing on all pages "completed" the plugins frontend flow

--- plugin/src/js/background.js
+++ plugin/src/js/background.js
@@ -7,17 +7,19 @@ import '../images/get_started32.png';
 import '../images/get_started48.png';
 import '../images/get_started128.png';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
     console.log("The color is green.");
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.meetup.com'},
+        pageUrl: {
+          schemes: ["http", "https"]
+        }
       })
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
--- plugin/src/js/parseEvent.js
+++ plugin/src/js/parseEvent.js
@@ -1,32 +1,66 @@
 import WAE from 'web-auto-extractor'
+import { ethers } from "ethers"
+import abi from './abi';
+import IPFS from 'ipfs'
 
 function DOMtoString(document_root) {
-  var html = '',
-      node = document_root.firstChild;
-  while (node) {
-      switch (node.nodeType) {
-      case Node.ELEMENT_NODE:
-          html += node.outerHTML;
-          break;
-      case Node.TEXT_NODE:
-          html += node.nodeValue;
-          break;
-      case Node.CDATA_SECTION_NODE:
-          html += '<![CDATA[' + node.nodeValue + ']]>';
-          break;
-      case Node.COMMENT_NODE:
-          html += '<!--' + node.nodeValue + '-->';
-          break;
-      case Node.DOCUMENT_TYPE_NODE:
-          // (X)HTML documents are identified by public identifiers
-          html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-          break;
-      }
-      node = node.nextSibling;
-  }
-  return html;
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE:
+                html += node.outerHTML;
+                break;
+            case Node.TEXT_NODE:
+                html += node.nodeValue;
+                break;
+            case Node.CDATA_SECTION_NODE:
+                html += '<![CDATA[' + node.nodeValue + ']]>';
+                break;
+            case Node.COMMENT_NODE:
+                html += '<!--' + node.nodeValue + '-->';
+                break;
+            case Node.DOCUMENT_TYPE_NODE:
+                // (X)HTML documents are identified by public identifiers
+                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+                break;
+        }
+        node = node.nextSibling;
+    }
+    return html;
 }
 
-var documentAsString = DOMtoString(document)
-var parsed = WAE().parse(documentAsString)
-console.log(parsed.jsonld.Event)
+const eventOnEth = async (url) => {
+    const provider = ethers.getDefaultProvider('ropsten');
+    const contractAddress = '0x55Fb10e4857293670446D2599A466E95fD5Ac0e8'; //'0xC4e47254c19102F55D8739181f9727FCE267564c';
+    const contract = new ethers.Contract(contractAddress, abi, provider);
+    const result = await contract.isRegistered(url)
+    return result;
+}
+
+const addToIpfs = async (ipfs, payload) => {
+    const [{ hash }] = await ipfs.add(payload)
+    const data = await ipfs.cat(hash)
+    console.log(data)
+    return hash
+}
+IPFS.create({ repo: String(Math.random() + Date.now()) }).then(ipfs => {
+    var documentAsString = DOMtoString(document)
+    var parsed = WAE().parse(documentAsString)
+    if (parsed.jsonld && parsed.jsonld.Event) {
+        // console.log(parsed.jsonld.Event)
+        const url = document.location.href
+        eventOnEth(url).then(res => {
+            if (res === true) {
+                alert("this event already is tracked on Eth. Join it!")
+            } else {
+                addToIpfs(ipfs, JSON.stringify(parsed.jsonld.Event)).then(hash => {
+                    alert("this event is not tracked on Eth yet. We added its metadata" + hash)
+                    //const data = await node.cat(hash)
+                })
+            }
+        })
+    }
+})
+
+
